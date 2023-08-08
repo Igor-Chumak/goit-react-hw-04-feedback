da539740b0582678058139917f3b8cc22074752d
Igor <125488812+Igor-Chumak@users.noreply.github.com>
2023-08-08 17:10:08 +0300

    refact ok

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyles, darkTheme, lightTheme, theme } from 'styles';
 import {
@@ -15,77 +15,70 @@ import typeFeedbacks from 'data/type_feedback.json';
 const stateDefault = {};
 typeFeedbacks.map(({ nameId, value }) => (stateDefault[nameId] = value));
 
-export class App extends Component {
-  state = {
-    ...stateDefault,
-    modeTheme: 'light',
+export const App = () => {
+  const [feedBack, setFeedBack] = useState(stateDefault);
+  const [modeTheme, setModeTheme] = useState('light');
+
+  const handleToggleTheme = () => {
+    setModeTheme(prevModeTheme =>
+      prevModeTheme === 'light' ? 'dark' : 'light'
+    );
   };
 
-  handleToggleTheme = () => {
-    this.setState(prevState => {
-      return {
-        modeTheme: prevState.modeTheme === 'light' ? 'dark' : 'light',
-      };
-    });
-  };
-
-  onLeaveFeedback = e => {
+  const onLeaveFeedback = e => {
     let stateKey = e.currentTarget.name;
-    this.setState(prevState => {
-      return { [stateKey]: prevState[stateKey] + 1 };
+    setFeedBack(prevState => {
+      return { ...feedBack, ...{ [stateKey]: prevState[stateKey] + 1 } };
     });
   };
 
-  countTotalFeedback = () => {
-    return Object.keys(this.state).reduce((previousValue, element) => {
-      return typeof this.state[element] === 'number'
-        ? previousValue + this.state[element]
-        : previousValue;
+  const countTotalFeedback = () => {
+    return Object.keys(feedBack).reduce((previousValue, element) => {
+      return previousValue + feedBack[element];
     }, 0);
   };
 
-  countPositiveFeedbackPercentage = () => {
-    return this.state.good > 0
-      ? Math.round((this.state.good / this.countTotalFeedback()) * 100)
+  const countPositiveFeedbackPercentage = () => {
+    return feedBack.good > 0
+      ? Math.round((feedBack.good / countTotalFeedback()) * 100)
       : 0;
   };
 
-  render() {
-    let total = this.countTotalFeedback();
-    return (
-      <ThemeProvider
-        theme={{
-          ...theme,
-          ...(this.state.modeTheme === 'light' ? lightTheme : darkTheme),
-        }}
-      >
-        <GlobalStyles />
-        <Header>
-          <CreateThemeSwitcher
-            handleToggleTheme={this.handleToggleTheme}
-            modeTheme={this.state.modeTheme === 'light' ? false : true}
+  let total = countTotalFeedback();
+
+  return (
+    <ThemeProvider
+      theme={{
+        ...theme,
+        ...(modeTheme === 'light' ? lightTheme : darkTheme),
+      }}
+    >
+      <GlobalStyles />
+      <Header>
+        <CreateThemeSwitcher
+          handleToggleTheme={handleToggleTheme}
+          modeTheme={modeTheme === 'light' ? false : true}
+        />
+      </Header>
+      <main>
+        <Section title="Please leave feedback">
+          <ButtonList
+            options={typeFeedbacks}
+            onLeaveFeedback={onLeaveFeedback}
           />
-        </Header>
-        <main>
-          <Section title="Please leave feedback">
-            <ButtonList
-              options={typeFeedbacks}
-              onLeaveFeedback={this.onLeaveFeedback}
+        </Section>
+        <Section title="Statistics">
+          {total > 0 ? (
+            <Statistics
+              state={feedBack}
+              total={total}
+              positivePercentage={countPositiveFeedbackPercentage()}
             />
-          </Section>
-          <Section title="Statistics">
-            {total > 0 ? (
-              <Statistics
-                state={this.state}
-                total={total}
-                positivePercentage={this.countPositiveFeedbackPercentage()}
-              />
-            ) : (
-              <Notification message="There is no feedback" />
-            )}
-          </Section>
-        </main>
-      </ThemeProvider>
-    );
-  }
-}
+          ) : (
+            <Notification message="There is no feedback" />
+          )}
+        </Section>
+      </main>
+    </ThemeProvider>
+  );
+};
